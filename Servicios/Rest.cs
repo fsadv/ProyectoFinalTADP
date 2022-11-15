27dc2f755c87955e9472b4a1e9018ddab96e54a3
Nicole Carda Andretto <Ni-ki_96@hotmail.com>
2022-11-14 21:53:38 -0300

    commit envio mail

--- Servicios/Rest.cs
+++ Servicios/Rest.cs
@@ -15,7 +15,7 @@ namespace Servicios
 
         public string Method { get; set; }
 
-        public Rest(string url, string data = "",string method="")
+        public Rest(string url, string data = "", string method = "GET")
         {
             URL = url;
             Data = data;
