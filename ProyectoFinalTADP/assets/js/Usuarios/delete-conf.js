d1d4948c28eca576bf28e05803fb693a219265c1
Facundo Sad <fsad@buenosaires.gob.ar>
2022-11-28 20:35:58 -0300

    Confirmacion de eliminado con alerta

---
+++ ProyectoFinalTADP/assets/js/Usuarios/delete-conf.js
@@ -0,0 +1,30 @@
+﻿$(document).ready(function () {
+
+    
+    $('.delete-conf').click((event) => {
+
+        event.preventDefault();
+
+        return Swal.fire({
+            title: '¿Está seguro??',
+            text: "Esta acción no puede ser revertida",
+            icon: 'warning',
+            showCancelButton: true,
+            confirmButtonColor: '#3085d6',
+            cancelButtonColor: '#d33',
+            confirmButtonText: 'Si, borrar!',
+            cancelButtonText: 'No, regresar'
+        }).then((result) => {
+            if (result.isConfirmed) {
+                Swal.fire(
+                    'Eliminado!',
+                    'El usuario fue eliminado.',
+                    'success'
+                )
+            }
+        })
+    
+
+    })
+
+})
