0a5b0734948f548f67e6549bb4085ae59eb7a29c
Facundo Sad <fsad@buenosaires.gob.ar>
2022-11-28 18:54:34 -0300

    Validacion jQuery-Ajax-JS en Login

---
+++ ProyectoFinalTADP/assets/js/Usuarios/login.js
@@ -0,0 +1,40 @@
+﻿$(document).ready(function () {
+
+    $('#submit').attr('disabled', 'disabled');
+
+    $('#mail').blur(() => {
+        $('#rellenar').hide()
+    })
+  
+    $('#password').blur(() => {
+
+        const mail = $('#mail').val();
+        const pass = $('#password').val();
+
+        
+        $.get("https://636c31077f47ef51e143fcb9.mockapi.io/Usuarios", (data) => {
+
+            const user = data.filter(x => x.Email == mail && x.Clave == pass);
+
+            if (user.length != 0) {
+                $('#submit').removeAttr('disabled');
+                $('#rellenar').hide()
+            } else {
+                $('#rellenar').show().html('Usuario o contraseña incorrectos')
+                $('#submit').attr('disabled', 'disabled');
+            }
+                        
+
+        })
+
+        
+        
+
+
+    })
+   
+
+
+
+})
+
