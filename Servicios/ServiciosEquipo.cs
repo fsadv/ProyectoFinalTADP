c875c7eeba82cbb6da1f694872099fadd4766f6c
Nicole Carda Andretto <Ni-ki_96@hotmail.com>
2022-11-29 17:08:52 -0300

    servicios dinamicos

---
+++ Servicios/ServiciosEquipo.cs
@@ -0,0 +1,25 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Servicios
+{
+    public class ServiciosEquipo
+    {
+        public static List<Entidades.ServicioEquipo> Listar()
+        {
+            string listServiciosJson = new Rest("https://63853e57beaa6458265c21fb.mockapi.io/Servicios").CreateObject();
+            List<Entidades.ServicioEquipo> listServicios = JsonConvert.DeserializeObject<List<Entidades.ServicioEquipo>>(listServiciosJson);
+
+            return listServicios;
+
+
+        }
+
+    }
+
+}
