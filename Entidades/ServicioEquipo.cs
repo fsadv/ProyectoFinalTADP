c77d34afc1fc87408a24ce4c80422842bf2ca737
Zycness <kevinflores2322002@gmail.com>
2022-12-05 15:09:33 -0300

    servicio iconos

--- Entidades/ServicioEquipo.cs
+++ Entidades/ServicioEquipo.cs
@@ -1,6 +1,7 @@
 ﻿using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -15,6 +16,10 @@ namespace Entidades
 
         [JsonProperty("Descripcion")]
         public string Descripcion { get; set; }
+
+        [JsonProperty("UrlIconoServicio")]
+        [DataType(DataType.ImageUrl)]
+        public string UrlIconoServicio { get; set; }
 
     }
 }
