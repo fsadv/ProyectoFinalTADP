96d2a2194aeda7aab80a3f4af2ba5ee469fb8635
Nicole Carda Andretto <Ni-ki_96@hotmail.com>
2022-12-12 22:21:21 -0300

    validaciones crear

--- ProyectoFinalTADP/assets/js/Usuarios/create-user.js
+++ ProyectoFinalTADP/assets/js/Usuarios/create-user.js
@@ -1,11 +1,24 @@
 ﻿$(document).ready(function () {
 
 
+    const regexMail = /\S+@\S+\.\S+/;
+
+    $('#Email').unbind().blur(() => {
+
+        const Email = $('#Email').val();
+
+        if (!regexMail.test(Email)) {
+
+            
+            Swal.fire('Por favor ingrese un mail correcto');
+            $('#Email').val("");
+            
+        }
+    })
+
     $('#btnCrear').click((event) => {
 
-
         event.preventDefault();
-
 
         const obj = {
             'Nombre': $('#Nombre').val(),
@@ -16,35 +29,61 @@
             'Descripcion': $('#Descripcion').val()
         }
 
-        $.ajax({
-            url: 'https://636c31077f47ef51e143fcb9.mockapi.io/Usuarios',
-            type: 'POST',
-            data: obj,
-            success: () => {
+        let CampoVacio = ValidarCampos(obj);
 
-                Swal.fire({
-                    position: 'center',
-                    icon: 'success',
-                    title: 'Your work has been saved',
-                    showConfirmButton: true,
-                    timer: 1500
-                })
+        if (!CampoVacio) {
+
+                            $.ajax({
+                                url: 'https://636c31077f47ef51e143fcb9.mockapi.io/Usuarios',
+                                type: 'POST',
+                                data: obj,
+                                success: () => {
+
+                                    Swal.fire({
+                                        position: 'center',
+                                        icon: 'success',
+                                        title: 'IUsuario guardado con éxito',
+                                        showConfirmButton: true,
+                                        timer: 1500
+                                    })
+                                }
+                            })
+
+                            setTimeout(function () {
+                                location.reload()
+                            },2000)               
+
+        } else {
+
+            Swal.fire({
+                icon: 'error',
+                title: 'Campo vacío',
+                text: 'Hay algún campo vacío!',
+            })
+        }
+
+
+    function ValidarCampos(obj) {
+
+        let errores = false;
+
+        for (const prop in obj) {
+
+          if (!obj[prop]) {
+       
+              errores = true;
+      
             }
-        })
-
-        setTimeout(function () {
-            location.reload()
-        },2000)      
+        }
 
 
-                
+        return errores;
 
-               
+    }
 
-    })
 })
 
 
-
+})
    
 
